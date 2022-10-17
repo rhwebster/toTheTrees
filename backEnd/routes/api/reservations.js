bce39a298c3571d28cdd3c7f30a6e8e325f6783d
Ryan Webster <ryan.h.webster@gmail.com>
2022-10-17 16:42:30 -0700

    reservations

--- backEnd/routes/api/reservations.js
+++ backEnd/routes/api/reservations.js
@@ -4,6 +4,28 @@ const { Reservation } = require('../../db/models');
 
 const router = express.Router();
 
+// Get user's bookings
 router.get('/user/:userId', asyncHandler(async (req, res) => {
-    
-}))
+    const userId = req.params.userId;
+    const reservations = await Reservation.findAll({
+        where: {
+            userId,
+        },
+    });
+    return res.json(reservations);
+}));
+
+// Create a new res
+router.post('/', asyncHandler(async (req, res) => {
+    const resy = await Reservation.create(req.body);
+    return res.json(resy);
+}));
+
+router.delete('/:reservationId', asyncHandler(async (req, res) => {
+    const resyId = req.params.reservationId;
+    const resy = await Reservation.findByPk(resyId);
+    await resy.destroy();
+    return res.json(resyId);
+}));
+
+module.exports = router;
