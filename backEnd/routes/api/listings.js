a7da4e313f3bfbb819aca3eb231b66cdebf37f24
Ryan Webster <ryan.h.webster@gmail.com>
2022-10-17 13:24:31 -0700

    listing routes

--- backEnd/routes/api/listings.js
+++ backEnd/routes/api/listings.js
@@ -1,20 +1,89 @@
 const express = require("express");
 const asyncHandler = require ('express-async-handler');
 const { restoreUser } = require('../../utils/auth');
-const db = require('../../db/models');
+const { Listing, User, GuestReview, Reservation, Favorite } = require('../../db/models');
 const { Op } = require('sequelize');
+const listing = require("../../db/models/listing");
 
 const router = express.Router();
 
-const { Listing } = require('../../db/models');
-
-router.get("/", async (req, res) => {
+// all listings
+router.get('/listings', asyncHandler(async (req, res) => {
     try {
-        const listings = await Listing.findAll();
-        res.json({listings: listings});
+        const listings = await Listing.findAll({
+            include: [User],
+        });
+        return res.json({ listings });
     } catch (e) {
         next(e);
     }
-})
+}));
+
+// one listing
+router.get('/listings/:listingId', asyncHandler(async (req, res) => {
+    const listingId = req.params.listingId;
+    const spot = await Listing.findByPk(Number(listingId));
+    return res.json({ spot });
+}))
+
+// all listings posted by a specific user
+router.get('/user/:userId', asyncHandler(async (req, res) => {
+    const user = req.params.userId;
+    const userSpots = await Listing.findAll({
+        where: {
+            userId: user,
+        },
+        include: User,
+    });
+    return res.json({ userSpots });
+}));
+
+// create new listing
+router.post('/', asyncHandler(async(req, res) => {
+    const newListing = await Listing.create(req.body);
+    return res.json(newListing);
+}));
+
+// edit listing
+router.put('/:listingId', asyncHandler(async (req, res) => {
+    const listingId = req.params.listingId;
+    const editedSpot = await Listing.findByPk(listingId, {
+        include: User,
+    });
+    await editedSpot.update(req.body);
+    return res.json(editedSpot);
+}))
+
+// deleting listing
+router.delete('/:listingId', asyncHandler(async (req, res) => {
+    const listingId = req.params.listingId;
+    const listingReviews = await GuestReview.findAll({
+        where: {
+            listingId,
+        },
+    });
+    listingReviews.forEach(async (review) => {
+        await review.destroy();
+    })
+    const listings = await Listing.findAll({
+        where: {
+            listingId,
+        },
+    });
+    listings.forEach(async (listing) => {
+        await listing.destroy();
+    })
+    const favorites = await Favorite.findAll({
+        where: {
+            listingId,
+        },
+    });
+    favorites.forEach(async (favorite) => {
+        await favorite.destroy();
+    });
+    const spot = await Listing.findByPk(listingId);
+    await spot.destroy();
+    return res.json(spot);
+}));
 
 module.exports = router;
